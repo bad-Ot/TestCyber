8cce164156cea0c7c9f427b9ccb79415d54e6382
badrotarid <badrotarid@gmail.com>
2026-03-17 11:23:39 +0100

    test6

--- 1/test.php
+++ 1/test.php
@@ -2,14 +2,12 @@
 
 require_once('../_helpers/strip.php');
 
-// https://depthsecurity.com/blog/exploitation-xml-external-entity-xxe-injection
-
-libxml_disable_entity_loader (false);
-
-$xml = strlen($_GET['xml']) > 0 ? $_GET['xml'] : '<root><content>No XML found</content></root>';
+$xml = isset($_GET['xml']) && strlen($_GET['xml']) > 0
+    ? $_GET['xml']
+    : '<root><content>No XML found</content></root>';
 
 $document = new DOMDocument();
-$document->loadXML($xml)
+$document->loadXML($xml);
 $parsedDocument = simplexml_import_dom($document);
 
 echo $parsedDocument->content;
