812c939e76f55ff42b4e5ca15e8d0c3aec36c6c7
badrotarid <badrotarid@gmail.com>
2026-03-17 11:18:50 +0100

    test5

--- 1/XmlReader_Tests.cs
+++ 1/XmlReader_Tests.cs
@@ -16,8 +16,8 @@ namespace XXEExamples.Tests
             AssertXXE.IsXMLParserSafe((string xml) =>
             {
                 XmlReaderSettings settings = new XmlReaderSettings();
-                settings.DtdProcessing = DtdProcessing.Parse;
-                settings.XmlResolver = new XmlUrlResolver();
+                settings.DtdProcessing = DtdProcessing.Prohibit;
+                settings.XmlResolver = null;
                 settings.MaxCharactersFromEntities = 6000;
 
                 using (MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(xml)))
@@ -74,7 +74,7 @@ namespace XXEExamples.Tests
                         XmlReader reader = XmlReader.Create(stream, settings);
 
                         var xmlDocument = new XmlDocument();
-                        xmlDocument.XmlResolver = new XmlUrlResolver();
+                        xmlDocument.XmlResolver = null;
                         xmlDocument.Load(reader);
                         return xmlDocument.InnerText;
                     }
